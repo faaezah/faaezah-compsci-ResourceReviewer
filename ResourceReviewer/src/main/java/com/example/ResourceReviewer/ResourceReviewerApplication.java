28135234798a872699835fae2be355835fb6e6a4
faaezah <faaezah.ahmed@gmail.com>
2026-05-26 21:06:20 -0500

    Initial commit

---
+++ ResourceReviewer/src/main/java/com/example/ResourceReviewer/ResourceReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResourceReviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceReviewerApplication.class, args);
+	}
+
+}
